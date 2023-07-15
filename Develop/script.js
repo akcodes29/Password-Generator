2d7bf6630f6311ca65aaa4d77c14e6ffc9cba868
Andrea <keriazes.andrea@gmail.com>
2023-07-15 14:48:06 -0400

    added comments before beginning

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,21 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  console.log("Button Clicked")
+
+  //1. Prompt the user for the password criteria.
+  //  a. Password length 8 < 128
+  //  b. Lowercase, uppercase, special characters, numbers
+  //2. Validate the input. 
+  //3. Generate password based on criteria.
+
+  
+  //4. Display the password on the page.
+
+  return "Generated password will go here";
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
