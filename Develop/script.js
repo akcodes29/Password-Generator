07553e7611461ec844206e8d968e4278f9f1754e
Andrea <keriazes.andrea@gmail.com>
2023-07-16 23:46:15 -0400

    added the rest of the capital alphabet

--- Develop/script.js
+++ Develop/script.js
@@ -12,7 +12,7 @@ var characterArr = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", 
 
 var lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-var upperCaseArr= ["A", "B", "C", "D", "E", "F", "G", "H"];
+var upperCaseArr= ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
 //combine to be used to concate 
 var combine= [];
